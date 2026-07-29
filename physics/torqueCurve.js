ef4dda56c69b98ff7a6e1b867b3287fd1be7565a
killian18-R1 <killianazzoug18@gmail.com>
2026-07-29 18:59:06 +0200

    v0.0.5 - Ajout courbe de couple moteur / torqueCurve.js
    - Création du module torqueCurve
    - Ajout d'une courbe couple/RPM
    - Ajout interpolation entre les points
    - Ajout calcul puissance instantanée
    - Préparation moteur multi-véhicules

--- physics/torqueCurve.js
+++ physics/torqueCurve.js
@@ -7,71 +7,225 @@
 */
 
 
+/*
+    Courbe moteur prototype
+
+    RPM     Couple Nm
+
+    1300    40
+    2500    58
+    4000    74
+    6000    92
+    8000    105
+    9500    110
+    11000   108
+    12500   101
+    14000   92
+
+*/
+
+
 const torqueCurve = [
 
-    { rpm: 1300, torque: 40 },
 
-    { rpm: 2500, torque: 58 },
+    {
+        rpm:1300,
+        torque:40
+    },
 
-    { rpm: 4000, torque: 74 },
 
-    { rpm: 6000, torque: 92 },
+    {
+        rpm:2500,
+        torque:58
+    },
 
-    { rpm: 8000, torque: 105 },
 
-    { rpm: 9500, torque: 110 },
+    {
+        rpm:4000,
+        torque:74
+    },
 
-    { rpm: 11000, torque: 108 },
 
-    { rpm: 12500, torque: 101 },
+    {
+        rpm:6000,
+        torque:92
+    },
 
-    { rpm: 14000, torque: 92 }
+
+    {
+        rpm:8000,
+        torque:105
+    },
+
+
+    {
+        rpm:9500,
+        torque:110
+    },
+
+
+    {
+        rpm:11000,
+        torque:108
+    },
+
+
+    {
+        rpm:12500,
+        torque:101
+    },
+
+
+    {
+        rpm:14000,
+        torque:92
+    }
+
 
 ];
 
 
 
+
+
+
+
+/*
+============================================================
+ Recherche du couple disponible selon RPM
+============================================================
+*/
+
+
 function getTorque(currentRPM){
+
+
 
     if(currentRPM <= torqueCurve[0].rpm){
 
+
         return torqueCurve[0].torque;
+
 
     }
 
 
-    for(let i=0;i<torqueCurve.length-1;i++){
 
-        const p1 = torqueCurve[i];
 
-        const p2 = torqueCurve[i+1];
+    for(let i = 0; i < torqueCurve.length - 1; i++){
 
-        if(currentRPM >= p1.rpm && currentRPM <= p2.rpm){
 
-            const ratio =
 
-                (currentRPM-p1.rpm) /
+        let pointA = torqueCurve[i];
 
-                (p2.rpm-p1.rpm);
 
-            return p1.torque +
+        let pointB = torqueCurve[i+1];
 
-                (p2.torque-p1.torque)*ratio;
+
+
+
+
+        if(
+
+            currentRPM >= pointA.rpm &&
+
+            currentRPM <= pointB.rpm
+
+        ){
+
+
+
+            let ratio =
+
+
+            (
+
+                currentRPM - pointA.rpm
+
+            )
+
+            /
+
+            (
+
+                pointB.rpm - pointA.rpm
+
+            );
+
+
+
+
+
+            return (
+
+                pointA.torque +
+
+                (
+
+                    pointB.torque -
+
+                    pointA.torque
+
+                )
+
+                *
+
+                ratio
+
+            );
+
 
         }
 
+
+
     }
 
+
+
+
+
     return torqueCurve[torqueCurve.length-1].torque;
+
+
 
 }
 
 
 
+
+
+
+
+/*
+============================================================
+ Calcul puissance moteur instantanée
+
+ P(ch)=Couple(Nm)*RPM/7127
+============================================================
+*/
+
+
 function getPowerHP(currentRPM){
 
-    const torque = getTorque(currentRPM);
 
-    return (torque * currentRPM) / 7127;
+
+    let torque = getTorque(currentRPM);
+
+
+
+    return (
+
+        torque *
+
+        currentRPM
+
+    )
+
+    /
+
+    7127;
+
+
 
 }
